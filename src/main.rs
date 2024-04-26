1c4ac1d60162d9355d3c36890fbf6cac4640b64f
RetricSu <inneverland2013@gmail.com>
2024-04-26 22:49:33 +0800

    Update small UI (#2)
    * update small uis
    
    * use top pannels

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use chrono::{DateTime, Local};
-use eframe::egui;
+use eframe::egui::{self, Align, Color32, Layout};
 use types::Task;
 
 mod store;
@@ -44,7 +44,14 @@ impl Default for MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        egui::TopBottomPanel::top("top-panel").show(ctx, |ui| {
+            let faded_color = ui.visuals().window_fill();
+            let faded_color = |color: Color32| -> Color32 {
+                use egui::Rgba;
+                let t = { 0.8 };
+                egui::lerp(Rgba::from(color)..=Rgba::from(faded_color), t).into()
+            };
+
             let now: DateTime<Local> = Local::now();
             let date_string = now.format("%Y-%m-%d").to_string();
 
@@ -52,8 +59,17 @@ impl eframe::App for MyApp {
 
             ui.add_space(12.0);
 
+            // input area
             ui.horizontal(|ui| {
-                ui.add(egui::TextEdit::singleline(&mut self.new_task).desired_width(f32::INFINITY));
+                ui.with_layout(Layout::left_to_right(Align::Min), |ui| {
+                    ui.style_mut().visuals.extreme_bg_color =
+                        faded_color(Color32::from_white_alpha(9));
+                    ui.add(
+                        egui::TextEdit::multiline(&mut self.new_task)
+                            .frame(true)
+                            .desired_width(f32::INFINITY),
+                    );
+                });
 
                 if ui.input(|input| input.key_pressed(egui::Key::Enter)) {
                     self.todo_list.push(Task::new(self.new_task.clone()));
@@ -67,41 +83,65 @@ impl eframe::App for MyApp {
             });
 
             ui.add_space(12.0);
+        });
 
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let faded_color = ui.visuals().window_fill();
+            let faded_color = |color: Color32| -> Color32 {
+                use egui::Rgba;
+                let t = { 0.8 };
+                egui::lerp(Rgba::from(color)..=Rgba::from(faded_color), t).into()
+            };
             // Display todo list
             let mut tasks_to_remove = vec![];
 
-            for (index, task) in self
-                .todo_list
-                .iter_mut()
-                .filter(|todo| todo.is_today() || !todo.completed)
-                .enumerate()
-            {
-                ui.horizontal(|ui| {
-                    let description = task.description.clone();
-                    let mut completed = task.completed;
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                // Add a lot of widgets here.
 
-                    if ui.checkbox(&mut completed, "").clicked() {
-                        task.completed = completed;
+                for (index, task) in self
+                    .todo_list
+                    .iter_mut()
+                    .filter(|todo| todo.is_today() || !todo.completed)
+                    .enumerate()
+                {
+                    ui.horizontal(|ui| {
+                        let description = task.description.clone();
+                        let mut completed = task.completed;
 
-                        // update the completed_at
-                        if completed {
-                            let now = SystemTime::now();
-                            let completed_at = now
-                                .duration_since(UNIX_EPOCH)
-                                .expect("Time went backwards")
-                                .as_secs();
-                            task.completed_at = completed_at;
-                        } else {
-                            task.completed_at = 0;
-                        }
-                    }
-                    ui.label(description);
-                    if ui.button("Delete").clicked() {
-                        tasks_to_remove.push(index);
-                    }
-                });
-            }
+                        ui.columns(1, |cols| {
+                            cols[0].horizontal_centered(|ui| {
+                                if ui.checkbox(&mut completed, "").clicked() {
+                                    task.completed = completed;
+
+                                    // update the completed_at
+                                    if completed {
+                                        let now = SystemTime::now();
+                                        let completed_at = now
+                                            .duration_since(UNIX_EPOCH)
+                                            .expect("Time went backwards")
+                                            .as_secs();
+                                        task.completed_at = completed_at;
+                                    } else {
+                                        task.completed_at = 0;
+                                    }
+                                }
+                                ui.label(description);
+
+                                if ui
+                                    .add(
+                                        egui::Button::new("Delete")
+                                            .small()
+                                            .fill(faded_color(Color32::RED)),
+                                    )
+                                    .clicked()
+                                {
+                                    tasks_to_remove.push(index);
+                                }
+                            });
+                        });
+                    });
+                }
+            });
 
             // Remove tasks outside the loop
             for &index in tasks_to_remove.iter().rev() {
