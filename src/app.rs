ca1efde2ad9766926dc8733ae9f89c59a132b1ca
RetricSu <inneverland2013@gmail.com>
2024-04-28 16:38:18 +0800

    feat: add worklog history view

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,19 @@
+use std::collections::HashMap;
+use std::ops::Sub;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use super::store;
 use super::Task;
 
 use chrono::{DateTime, Local};
-use eframe::egui::{self, Align, Color32, Layout};
+use eframe::egui::{self, Align, Color32, Layout, Ui};
 
 pub struct MyApp {
     new_task: String,
     todo_list: Vec<Task>,
+    show_deferred_history: Arc<AtomicBool>,
 }
 
 impl Default for MyApp {
@@ -17,6 +22,7 @@ impl Default for MyApp {
         Self {
             new_task: "".to_owned(),
             todo_list: tasks,
+            show_deferred_history: Arc::new(AtomicBool::new(false)),
         }
     }
 }
@@ -34,7 +40,23 @@ impl eframe::App for MyApp {
             let now: DateTime<Local> = Local::now();
             let date_string = now.format("%Y-%m-%d").to_string();
 
-            ui.heading(&format!("{} {}", "🔆", date_string));
+            // header with title in the left and menu in the right
+            ui.horizontal(|ui| {
+                ui.columns(2, |columns| {
+                    columns[0].heading(&format!("{} {}", "🔆", date_string));
+
+                    columns[1].horizontal(|ui| {
+                        // Add a flexible space to push the next item to the rightmost side
+                        ui.add_space(ui.available_width().sub(100.));
+
+                        let mut show_deferred_viewport =
+                            self.show_deferred_history.load(Ordering::Relaxed);
+                        ui.checkbox(&mut show_deferred_viewport, "Show history");
+                        self.show_deferred_history
+                            .store(show_deferred_viewport, Ordering::Relaxed);
+                    });
+                });
+            });
 
             ui.add_space(12.0);
 
@@ -133,6 +155,35 @@ impl eframe::App for MyApp {
                 eprintln!("Failed to store tasks: {}", err);
             });
         });
+
+        // open the history viewport
+        if self.show_deferred_history.load(Ordering::Relaxed) {
+            let show_deferred_viewport = self.show_deferred_history.clone();
+            let todo_list: Vec<Task> = self.todo_list.clone();
+            ctx.show_viewport_deferred(
+                egui::ViewportId::from_hash_of("deferred_history_viewport"),
+                egui::ViewportBuilder::default()
+                    .with_title("Worklog History")
+                    .with_inner_size([400.0, 500.0]),
+                move |ctx, class| {
+                    assert!(
+                        class == egui::ViewportClass::Deferred,
+                        "This egui backend doesn't support multiple viewports"
+                    );
+                    if ctx.input(|i| i.viewport().close_requested()) {
+                        // Tell parent to close us.
+                        show_deferred_viewport.store(false, Ordering::Relaxed);
+                    }
+
+                    // show history
+                    egui::CentralPanel::default().show(ctx, |ui| {
+                        egui::ScrollArea::vertical().show(ui, |ui| {
+                            ui_history(ui, &todo_list);
+                        });
+                    });
+                },
+            );
+        }
     }
 
     fn save(&mut self, _storage: &mut dyn eframe::Storage) {
@@ -140,4 +191,47 @@ impl eframe::App for MyApp {
             eprintln!("Failed to store tasks: {}", err);
         });
     }
+}
+
+fn ui_history(ui: &mut Ui, tasks: &[Task]) {
+    // Group tasks by created_at_date
+    let mut tasks_by_date: HashMap<String, Vec<&Task>> = HashMap::new();
+    for task in tasks {
+        let date = task.created_at_date();
+        tasks_by_date.entry(date).or_default().push(task);
+    }
+
+    // Sort dates
+    let mut sorted_dates: Vec<_> = tasks_by_date.keys().collect();
+    sorted_dates.sort();
+
+    // Begin the UI layout
+    ui.vertical_centered(|ui| {
+        // Iterate through each date with tasks
+        for dates in &sorted_dates {
+            if let Some(tasks) = tasks_by_date.get(*dates) {
+                // Add a header for the date
+                ui.horizontal(|ui| {
+                    ui.label(egui::RichText::new(*dates).color(Color32::DARK_GREEN));
+                });
+
+                // Begin a table for tasks
+                ui.vertical(|ui| {
+                    // Add a row for each task
+                    for task in tasks.iter() {
+                        ui.horizontal(|ui| {
+                            let is_completed = if task.completed {
+                                "\u{2714}"
+                            } else {
+                                "\u{2795}"
+                            };
+                            ui.label(format!("{} {}", is_completed, task.description));
+                        });
+                    }
+                    ui.separator();
+                });
+                ui.add_space(12.0);
+            }
+        }
+    });
 }
